e15c194e64a2ef47c222554e6e120447c6a87c38
vNiqkz <97852466+vNiqkz@users.noreply.github.com>
2022-05-25 08:56:29 -0400

    update Main.php

--- src/vNiqkz/Main.php
+++ src/vNiqkz/Main.php
@@ -5,14 +5,29 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\network\mcpe\protocol\GameRulesChangedPacket;
 use pocketmine\network\mcpe\protocol\types\BoolGameRule;
+use pocketmine\utils\Config;
 class Main extends PluginBase implements Listener{
     public function onEnable(): void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
+    public static function getConfiguration($name){
+      return new Config($this->getDataFoolder().$name.".yml", Config::YAML);
+    }
+    public static function activate($player): void{
+      
+    }
+    public static function getStatusPlayer(String $playerName){
+      return Main::getConfiguration("player")->get($playerName);
+    }
     public function onPlayerJoin(PlayerJoinEvent $event): void{
+      
         $player = $event->getPlayer();
-    	$pk = new GameRulesChangedPacket();
-		$pk->gameRules = ["showcoordinates" => new BoolGameRule(true, false)];
-        $player->getNetworkSession()->sendDataPacket($pk);
+        $statusPlayer = Main::getStatusPlayer($player->getName())
+        if($statusPlayer){
+          $pk = new GameRulesChangedPacket();
+		      $pk->gameRules = ["showcoordinates" => new BoolGameRule(true, false)];
+          $player->getNetworkSession()->sendDataPacket($pk);
+        }
+    	  
     }
 }
